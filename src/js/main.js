28897460e46ca770c99fc732a39cd8641f8ea4bb
Marcin8013 <marcing2067@wp.pl>
2021-02-05 23:15:22 +0100

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger")
+const navList = document.querySelector(".nav__list")
+const navListItems = document.querySelectorAll(".nav__list-item-link")
+const body = document.body
+
+const navFunction = () => {
+    if (window.innerWidth < 1024) {
+        hamburger.classList.toggle("is-active")
+        navList.classList.toggle("nav__list--active")
+        body.classList.toggle("block-scroll")
+    }
+}
+
+navListItems.forEach(item => {
+    item.addEventListener("click", navFunction)
+})
+
+hamburger.addEventListener("click", navFunction)
