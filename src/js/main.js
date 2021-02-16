290f81b22fc4f848cd1e6ec7c3a47231c9a291ce
Marcin8013 <marcing2067@wp.pl>
2021-02-16 10:05:38 +0100

    Back footer styles

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav__list");
 const navListItems = document.querySelectorAll(".nav__list-item-link");
 const body = document.body;
-const buttonBar = document.querySelectorAll(".footer__bar-button")
 
 const navFunction = () => {
     if (window.innerWidth < 1024) {
@@ -11,24 +10,6 @@ const navFunction = () => {
         body.classList.toggle("block-scroll")
     }
 }
-
-const footerBarFunction = (id, e) => {
-    const box = document.querySelector(id);
-    box.classList.toggle("footer__box--show")
-    e.target.textContent === '+' ? e.target.textContent = '-' : e.target.textContent = '+';
-}
-
-document.querySelector("#footer-first-button").addEventListener("click", () => {
-    footerBarFunction('#footer-first-box', event)
-})
-
-document.querySelector("#footer-twice-button").addEventListener("click", () => {
-    footerBarFunction('#footer-twice-box', event)
-})
-
-document.querySelector("#footer-third-button").addEventListener("click", () => {
-    footerBarFunction('#footer-third-box', event)
-})
 
 navListItems.forEach(item => {
     item.addEventListener("click", navFunction)
