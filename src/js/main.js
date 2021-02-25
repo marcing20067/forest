220d5e76d3305ece3cad89ccf791125d69466379
Marcin8013 <marcing2067@wp.pl>
2021-02-25 21:45:19 +0100

    Fix email input bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,22 @@
-const hamburger = document.querySelector(".hamburger");
-const navList = document.querySelector(".nav__list");
-const navListItems = document.querySelectorAll(".nav__list-item-link");
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav__list');
+const navListItems = document.querySelectorAll('.nav__list-item-link');
 const body = document.body;
+const emailForm = document.querySelector('.form__box-email');
 
 const navFunction = () => {
     if (window.innerWidth < 1024) {
-        hamburger.classList.toggle("is-active")
-        navList.classList.toggle("nav__list--active")
-        body.classList.toggle("block-scroll")
+        hamburger.classList.toggle('is-active')
+        navList.classList.toggle('nav__list--active')
+        body.classList.toggle('block-scroll')
     }
 }
 
-navListItems.forEach(item => {
-    item.addEventListener("click", navFunction)
+emailForm.addEventListener('keyup', function () {
+    this.value !== '' ? this.classList.add('form__box-email--valid') : this.classList.remove('form__box-email--valid');
 })
-hamburger.addEventListener("click", navFunction)
+
+navListItems.forEach(item => {
+    item.addEventListener('click', navFunction)
+})
+hamburger.addEventListener('click', navFunction)
