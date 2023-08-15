2d913eacb7e64b24a0d787c6f2d97d8d8e68e4c0
Marcin8013 <marcing2067@wp.pl>
2023-08-15 08:19:26 +0200

    Change structure, delete dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,85 +5,89 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
 const clean = require('gulp-clean');
-const kit = require('gulp-kit');
 const browserSync = require('browser-sync').create();
+const kit = require('gulp-kit-2');
 const reload = browserSync.reload;
-
+const ghpages = require('gh-pages');
 const paths = {
-    html: './html/**/*.kit',
-    sass: './src/sass/**/*.scss',
-    js: './src/js/**/*.js',
-    img: './src/img/*',
-    dist: './dist',
-    sassDest: './dist/css',
-    jsDest: './dist/js',
-    imgDest: './dist/img',
+  img: './src/img/**',
+  html: './src/html/**/*.kit',
+  sass: './src/sass/**/*.scss',
+  js: './src/js/**/*.js',
+  imgDest: './dist/img',
+  sassDest: './dist/css',
+  jsDest: './dist/js',
+  dist: './dist',
+};
+
+function handleKits(done) {
+  src(paths.html)
+    .pipe(kit())
+    .pipe(dest(paths.dist))
+    .on('error', (e) => console.log(e));
+  done();
 }
 
 function sassCompiler(done) {
-    src(paths.sass)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cssnano())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write())
-        .pipe(dest(paths.sassDest));
-    done()
+  src(paths.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(dest(paths.sassDest));
+  done();
 }
 
 function javaScript(done) {
-    src(paths.js)
-        .pipe(sourcemaps.init())
-        .pipe(babel({ presets: ['@babel/env'] }))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write())
-        .pipe(dest(paths.jsDest));
-    done()
-}
-
-function convertImages(done) {
-    src(paths.img)
-        .pipe(imagemin())
-        .pipe(dest(paths.imgDest));
-    done()
-}
-
-function handleKits(done) {
-    src(paths.html)
-        .pipe(kit())
-        .pipe(dest('./'));
-    done()
+  src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(dest(paths.jsDest));
+  done();
 }
 
 function cleanStuff(done) {
-    src(paths.dist, { read: false })
-        .pipe(clean());
-    done()
+  src(paths.dist, { read: false }).pipe(clean());
+  done();
 }
 
 function startBrowserSync(done) {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: paths.dist,
+    },
+  });
 
-    done()
+  done();
 }
 
 function watchForChanges(done) {
-    watch('./*.html').on("change", reload);
-    watch([paths.html, paths.sass, paths.js], parallel(handleKits, sassCompiler, javaScript)).on("change", reload);
-    watch(paths.img, convertImages).on("change", reload);
-    done()
+  watch('./dist/*.html').on('change', reload);
+  watch(
+    [paths.html, paths.sass, paths.js],
+    parallel(handleKits, sassCompiler, javaScript)
+  ).on('change', reload);
+  done();
 }
 
+function moveImagesToDist(done) {
+  src(paths.img).pipe(dest(paths.imgDest));
+  done();
+}
 
-const mainFunctions = parallel(handleKits, sassCompiler, javaScript, convertImages)
-exports.cleanStuff = cleanStuff
-exports.default = series(mainFunctions, startBrowserSync, watchForChanges)
+function ghPages(done) {
+  ghpages.publish('dist', done);
+}
+
+const mainFunctions = parallel(sassCompiler, javaScript, moveImagesToDist);
+exports.cleanStuff = cleanStuff;
+exports.default = series(mainFunctions, startBrowserSync, watchForChanges);
+exports.build = parallel(handleKits, mainFunctions);
+exports.deploy = series(handleKits, mainFunctions, ghPages);
